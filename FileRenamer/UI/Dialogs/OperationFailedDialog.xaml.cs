cb7689cb48e428cdbdae877d66f821f865bdfc9e
高怡飞 <363301617@qq.com>
2023-10-13 22:15:18 +0800

    Move XamlUICommand to Xaml page
    Move XamlUICommand to Xaml page

--- FileRenamer/UI/Dialogs/OperationFailedDialog.xaml.cs
+++ FileRenamer/UI/Dialogs/OperationFailedDialog.xaml.cs
@@ -17,35 +17,35 @@ namespace FileRenamer.UI.Dialogs
     {
         public ObservableCollection<OperationFailedModel> OperationFailedList { get; } = new ObservableCollection<OperationFailedModel>();
 
-        // 复制异常信息
-        public XamlUICommand CopyCommand { get; } = new XamlUICommand();
-
         public OperationFailedDialog(ObservableCollection<OperationFailedModel> operationFailedList)
         {
             InitializeComponent();
 
-            CopyCommand.ExecuteRequested += (sender, args) =>
-            {
-                OperationFailedModel operationFailedItem = args.Parameter as OperationFailedModel;
-                if (operationFailedItem is not null)
-                {
-                    StringBuilder builder = new StringBuilder();
-                    builder.Append(Dialog.FileNameCopy);
-                    builder.AppendLine(operationFailedItem.FileName);
-                    builder.Append(Dialog.FilePathCopy);
-                    builder.AppendLine(operationFailedItem.FilePath);
-                    builder.Append(Dialog.ExceptionMessage);
-                    builder.AppendLine(operationFailedItem.Exception.Message);
-                    builder.Append(Dialog.ExceptionCode);
-                    builder.AppendLine(operationFailedItem.Exception.HResult.ToString());
-                    CopyPasteHelper.CopyToClipBoard(builder.ToString());
-                    new DataCopyNotification(this, DataCopyKind.OperationFailed, false).Show();
-                }
-            };
-
             foreach (OperationFailedModel operationFailedItem in operationFailedList)
             {
                 OperationFailedList.Add(operationFailedItem);
+            }
+        }
+
+        /// <summary>
+        /// 复制异常信息
+        /// </summary>
+        public void OnCopyExecuteRequested(XamlUICommand command, ExecuteRequestedEventArgs args)
+        {
+            OperationFailedModel operationFailedItem = args.Parameter as OperationFailedModel;
+            if (operationFailedItem is not null)
+            {
+                StringBuilder builder = new StringBuilder();
+                builder.Append(Dialog.FileNameCopy);
+                builder.AppendLine(operationFailedItem.FileName);
+                builder.Append(Dialog.FilePathCopy);
+                builder.AppendLine(operationFailedItem.FilePath);
+                builder.Append(Dialog.ExceptionMessage);
+                builder.AppendLine(operationFailedItem.Exception.Message);
+                builder.Append(Dialog.ExceptionCode);
+                builder.AppendLine(operationFailedItem.Exception.HResult.ToString());
+                CopyPasteHelper.CopyToClipBoard(builder.ToString());
+                new DataCopyNotification(this, DataCopyKind.OperationFailed, false).Show();
             }
         }
 
