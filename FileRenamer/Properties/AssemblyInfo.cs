96a74550e2f0709e06ee68c78de84c6eee5de561
高怡飞 <363301617@qq.com>
2023-10-16 22:35:31 +0800

    Add Microsoft.UI.Xaml Package
    Add Microsoft.UI.Xaml Package

--- FileRenamer/Properties/AssemblyInfo.cs
+++ FileRenamer/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("高怡飞")]
 [assembly: AssemblyCopyright("Copyright ©2023 高怡飞, All Rights Reserved.")]
 [assembly: AssemblyDescription("文件重命名工具")]
-[assembly: AssemblyFileVersion("1.3.1011.0")]
-[assembly: AssemblyInformationalVersion("1.3.1011.0")]
+[assembly: AssemblyFileVersion("1.3.1016.0")]
+[assembly: AssemblyInformationalVersion("1.3.1016.0")]
 [assembly: AssemblyProduct("文件重命名工具")]
 [assembly: AssemblyTitle("文件重命名工具")]
-[assembly: AssemblyVersion("1.3.1011.0")]
+[assembly: AssemblyVersion("1.3.1016.0")]
 
 // 设置程序集对COM组件的访问权限
 [assembly: ComVisible(false)]
