b8cb71794b1fc093936d0e19fcb06212310e7fe3
高怡飞 <363301617@qq.com>
2025-02-03 01:19:57 +0800

    Add need elevated information prompt
    Add need elevated information prompt

---
+++ WindowsTools/UI/Dialogs/NeedElevatedDialog.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Windows.UI.Xaml.Controls;
+
+// 抑制 CA1822，IDE0060 警告
+#pragma warning disable CA1822,IDE0060
+
+namespace WindowsTools.UI.Dialogs
+{
+    /// <summary>
+    /// 需要提权信息对话框
+    /// </summary>
+    public sealed partial class NeedElevatedDialog : ContentDialog
+    {
+        public NeedElevatedDialog()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 提权运行该应用
+        /// </summary>
+        private void OnElevatedOpenClicked(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+        {
+            Task.Run(() =>
+            {
+                try
+                {
+                    ProcessStartInfo startInfo = new()
+                    {
+                        UseShellExecute = true,
+                        WorkingDirectory = Environment.CurrentDirectory,
+                        FileName = Path.GetFileName(Application.ExecutablePath),
+                        Verb = "runas"
+                    };
+                    Process.Start(startInfo);
+                }
+                catch
+                {
+                    return;
+                }
+            });
+        }
+    }
+}
