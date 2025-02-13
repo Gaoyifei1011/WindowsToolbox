e1e8a45e3a636d5061daf9096d3c18682300b9c7
高怡飞 <363301617@qq.com>
2025-02-13 15:49:56 +0800

    Finish task and publish 3.6.213.0 version
    Finish task and publish 3.6.213.0 version

--- WindowsTools/Properties/AssemblyInfo.cs
+++ WindowsTools/Properties/AssemblyInfo.cs
@@ -3,13 +3,13 @@ using System.Resources;
 
 // 程序集信息设置
 [assembly: AssemblyCompany("高怡飞")]
-[assembly: AssemblyCopyright("Copyright ©2024 高怡飞, All Rights Reserved.")]
+[assembly: AssemblyCopyright("Copyright 2022-2025 高怡飞 版权所有")]
 [assembly: AssemblyDescription("Windows 工具箱")]
-[assembly: AssemblyFileVersion("3.5.211.0")]
-[assembly: AssemblyInformationalVersion("3.5.211.0")]
+[assembly: AssemblyFileVersion("3.6.213.0")]
+[assembly: AssemblyInformationalVersion("3.6.213.0")]
 [assembly: AssemblyProduct("Windows 工具箱")]
 [assembly: AssemblyTitle("Windows 工具箱")]
-[assembly: AssemblyVersion("3.5.211.0")]
+[assembly: AssemblyVersion("3.6.213.0")]
 
 // 应用程序默认区域性的资源控制器设置
 [assembly: NeutralResourcesLanguage("en-us")]
