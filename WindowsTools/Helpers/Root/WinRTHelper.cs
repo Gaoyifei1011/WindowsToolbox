e44cb197ecaad15f8790c7a96a042c9842c2352d
高怡飞 <363301617@qq.com>
2025-02-24 16:33:33 +0800

    Add WinRTHelper
    Add WinRTHelper

---
+++ WindowsTools/Helpers/Root/WinRTHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using WindowsTools.WindowsAPI.PInvoke.Combase;
+
+// 抑制 CA1806 警告
+#pragma warning disable CA1806
+
+namespace WindowsTools.Helpers.Root
+{
+    /// <summary>
+    /// WinRT 辅助类
+    /// </summary>
+    public static class WinRTHelper
+    {
+        /// <summary>
+        /// 获取指定运行时类的激活工厂。
+        /// </summary>
+        public static object GetActivationFactory(string activatableClassId, Guid iid)
+        {
+            if (string.IsNullOrEmpty(activatableClassId) || iid.Equals(Guid.Empty))
+            {
+                return null;
+            }
+            else
+            {
+                if (CombaseLibrary.RoGetActivationFactory(activatableClassId, iid, out object comp) is 0)
+                {
+                    return comp;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+    }
+}
