dc32ffcd687fcad25bb0c23eb74c249f3eacda77
高怡飞 <363301617@qq.com>
2025-02-24 16:33:43 +0800

    Update BackdropHelper.cs

--- WindowsTools/Helpers/Backdrop/BackdropHelper.cs
+++ WindowsTools/Helpers/Backdrop/BackdropHelper.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using Windows.Foundation;
+﻿using Windows.Foundation;
+using WindowsTools.Helpers.Root;
 using WindowsTools.WindowsAPI.ComTypes;
-using WindowsTools.WindowsAPI.PInvoke.Combase;
 
 // 抑制 CA1806 警告
 #pragma warning disable CA1806
@@ -13,22 +12,6 @@ namespace WindowsTools.Helpers.Backdrop
     /// </summary>
     public static class BackdropHelper
     {
-        public static IPropertyValueStatics PropertyValueStatics { get; } = GetActivationFactory<IPropertyValueStatics>(typeof(PropertyValue).FullName, typeof(IPropertyValueStatics).GUID);
-
-        /// <summary>
-        /// 获取指定运行时类的激活工厂。
-        /// </summary>
-        public static T GetActivationFactory<T>(string activatableClassId, Guid iid)
-        {
-            if (!string.IsNullOrEmpty(activatableClassId))
-            {
-                CombaseLibrary.RoGetActivationFactory(activatableClassId, iid, out object comp);
-                return (T)comp;
-            }
-            else
-            {
-                return default;
-            }
-        }
+        public static IPropertyValueStatics PropertyValueStatics { get; } = (IPropertyValueStatics)WinRTHelper.GetActivationFactory(typeof(PropertyValue).FullName, typeof(IPropertyValueStatics).GUID);
     }
 }
