544d441162c9fb834e5bf41d96480011aaf39626
高怡飞 <363301617@qq.com>
2025-03-01 22:38:29 +0800

    Initialize WindowsTools properties
    Initialize WindowsTools properties

--- WindowsToolsSystemTray/Program.cs
+++ WindowsToolsSystemTray/Program.cs
@@ -1,17 +1,62 @@
 using System;
-using System.Windows.Forms;
+using System.Collections.Specialized;
+using System.Configuration;
+using System.Diagnostics;
+using System.Diagnostics.Tracing;
+using System.Globalization;
+using System.Threading;
+using WindowsToolsSystemTray.Helpers.Root;
+using WindowsToolsSystemTray.Services.Controls.Settings;
+using WindowsToolsSystemTray.Services.Root;
 
 namespace WindowsToolsSystemTray
 {
     public static class Program
     {
+        private static readonly NameValueCollection configurationCollection = ConfigurationManager.GetSection("System.Windows.Forms.ApplicationConfigurationSection") as NameValueCollection;
+
         /// <summary>
-        ///  The main entry point for the application.
+        /// Windows 工具箱 托盘程序
         /// </summary>
         [STAThread]
         public static void Main()
         {
-            Application.Run(new Form1());
+            if (!RuntimeHelper.IsMSIX)
+            {
+                try
+                {
+                    Process.Start("windowstools:");
+                }
+                catch (Exception)
+                { }
+                return;
+            }
+
+            InitializeProgramResources();
+
+            configurationCollection["DpiAwareness"] = "PerMonitorV2";
+            AppDomain.CurrentDomain.UnhandledException += OnUnhandledException;
+        }
+
+        /// <summary>
+        /// 处理 Windows 窗体非 UI 线程异常
+        /// </summary>
+        private static void OnUnhandledException(object sender, UnhandledExceptionEventArgs args)
+        {
+            LogService.WriteLog(EventLevel.Warning, "Background thread Exception", args.ExceptionObject as Exception);
+        }
+
+        /// <summary>
+        /// 加载应用程序所需的资源
+        /// </summary>
+        private static void InitializeProgramResources()
+        {
+            LogService.Initialize();
+            LanguageService.InitializeLanguage();
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo(LanguageService.AppLanguage.Key);
+            ResourceService.LocalizeReosurce();
+
+            ThemeService.InitializeTheme();
         }
     }
 }
---
+++ WindowsToolsSystemTray/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Resources;
+
+// 程序集信息设置
+[assembly: AssemblyCompany("高怡飞")]
+[assembly: AssemblyCopyright("Copyright 2022-2025 高怡飞 版权所有")]
+[assembly: AssemblyDescription("Windows 工具箱 托盘程序")]
+[assembly: AssemblyFileVersion("3.6.215.0")]
+[assembly: AssemblyInformationalVersion("3.6.215.0")]
+[assembly: AssemblyProduct("Windows 工具箱 托盘程序")]
+[assembly: AssemblyTitle("Windows 工具箱 托盘程序")]
+[assembly: AssemblyVersion("3.6.215.0")]
+
+// 应用程序默认区域性的资源控制器设置
+[assembly: NeutralResourcesLanguage("en-us")]
