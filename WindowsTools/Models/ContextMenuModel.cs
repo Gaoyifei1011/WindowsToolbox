0692404d2046e83838404888dc65aa96f1782cec
高怡飞 <363301617@qq.com>
2025-03-06 22:43:57 +0800

    Remove unused property
    Remove unused property

--- WindowsTools/Models/ContextMenuModel.cs
+++ WindowsTools/Models/ContextMenuModel.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
-using Windows.UI.Xaml;
 using Windows.UI.Xaml.Media;
 
 namespace WindowsTools.Models
@@ -9,24 +8,8 @@ namespace WindowsTools.Models
     /// <summary>
     /// 右键菜单项数据模型
     /// </summary>
-    public class ContextMenuModel : INotifyPropertyChanged
+    public class ContextMenuModel
     {
-        private Visibility _isVisible;
-
-        public Visibility IsVisible
-        {
-            get { return _isVisible; }
-
-            set
-            {
-                if (!Equals(_isVisible, value))
-                {
-                    _isVisible = value;
-                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsVisible)));
-                }
-            }
-        }
-
         public ImageSource PackageIcon { get; set; }
 
         public Uri PackageIconUri { get; set; }
