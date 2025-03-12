6a53a3290d6e4a6cb6c1bf68a7f377be558f3175
高怡飞 <363301617@qq.com>
2025-03-12 22:52:04 +0800

    Add DriverSortRuleKind enum
    Add DriverSortRuleKind enum

---
+++ WindowsTools/Extensions/DataType/Enums/DriverSortRuleKind.cs
@@ -0,0 +1,16 @@
+﻿namespace WindowsTools.Extensions.DataType.Enums
+{
+    /// <summary>
+    /// 驱动排序规则
+    /// </summary>
+    public enum DriverSortRuleKind
+    {
+        DeviceName = 0,
+        InfName = 1,
+        OEMInfName = 2,
+        DeviceType = 3,
+        Manufacturer = 4,
+        Version = 5,
+        Date = 6
+    }
+}
