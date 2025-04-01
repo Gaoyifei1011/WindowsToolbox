dc4ba0569d1bc05b62aa6ac94d7e634c02c6071a
高怡飞 <363301617@qq.com>
2025-04-01 22:54:31 +0800

    Import more Setupapi.dll API
    Import more Setupapi.dll API

---
+++ WindowsTools/WindowsAPI/PInvoke/Setupapi/SUOI_Flags.cs
@@ -0,0 +1,14 @@
+﻿namespace WindowsTools.WindowsAPI.PInvoke.Setupapi
+{
+    public enum SUOI_Flags
+    {
+        SUOI_NONE = 0x0000,
+
+        /// <summary>
+        /// SetupUninstallOEMInf 函数首先检查是否使用 .inf 文件安装了任何设备。 无需将设备作为使用 .inf 文件进行检测。
+        /// 如果未设置此标志，并且该函数查找使用此 .inf 文件安装的当前安装设备，则不会删除 .inf 文件。
+        /// 如果设置了此标志，则会删除 .inf 文件，该函数是否查找随此 .inf 文件一起安装的设备。
+        /// </summary>
+        SUOI_FORCEDELETE = 0x0001
+    }
+}
