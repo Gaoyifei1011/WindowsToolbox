339a6058da15907c96232d24d4f4dbcb40dab73c
高怡飞 <363301617@qq.com>
2025-04-03 00:49:01 +0800

    Add DriverOperationModel
    Add DriverOperationModel

---
+++ WindowsTools/Models/DriverOperationModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace WindowsTools.Models
+{
+    /// <summary>
+    /// 驱动操作数据模型
+    /// </summary>
+    public class DriverOperationModel : INotifyPropertyChanged
+    {
+        private bool _isOperating;
+
+        public bool IsOperating
+        {
+            get { return _isOperating; }
+
+            set
+            {
+                if (!Equals(_isOperating, value))
+                {
+                    _isOperating = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsOperating)));
+                }
+            }
+        }
+
+        /// <summary>
+        /// 驱动 INF 名称
+        /// </summary>
+        public string DriverInfName { get; set; }
+
+        /// <summary>
+        /// 驱动操作信息
+        /// </summary>
+        public string DriverOperation { get; set; }
+
+        /// <summary>
+        /// 驱动操作唯一 ID
+        /// </summary>
+        public Guid DriverOperationGuid { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
