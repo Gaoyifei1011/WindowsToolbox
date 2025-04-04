124f799758f15b5414bada45438fd99c49252f8a
高怡飞 <363301617@qq.com>
2025-04-04 21:56:00 +0800

    Update test version
    Update test version

--- WindowsToolsSystemTray/Properties/AssemblyInfo.cs
+++ WindowsToolsSystemTray/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Resources;
 [assembly: AssemblyCompany("高怡飞")]
 [assembly: AssemblyCopyright("Copyright 2022-2025 高怡飞 版权所有")]
 [assembly: AssemblyDescription("Windows 工具箱 托盘程序")]
-[assembly: AssemblyFileVersion("3.8.403.0")]
-[assembly: AssemblyInformationalVersion("3.8.403.0")]
+[assembly: AssemblyFileVersion("3.8.404.0")]
+[assembly: AssemblyInformationalVersion("3.8.404.0")]
 [assembly: AssemblyProduct("Windows 工具箱 托盘程序")]
 [assembly: AssemblyTitle("Windows 工具箱 托盘程序")]
-[assembly: AssemblyVersion("3.8.403.0")]
+[assembly: AssemblyVersion("3.8.404.0")]
 
 // 应用程序默认区域性的资源控制器设置
 [assembly: NeutralResourcesLanguage("en-us")]
