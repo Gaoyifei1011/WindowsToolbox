39f1c666f03bb5b61755f2cdb94857c2a0ee6a24
高怡飞 <363301617@qq.com>
2025-06-12 17:30:32 +0800

    Update RegistryHelper Equals method
    Update RegistryHelper Equals method

--- PowerTools/Helpers/Root/RegistryHelper.cs
+++ PowerTools/Helpers/Root/RegistryHelper.cs
@@ -27,33 +27,33 @@ namespace PowerTools.Helpers.Root
                 if ((Equals(rootRegistryKey, Registry.ClassesRoot) || Equals(rootRegistryKey, Registry.CurrentConfig) || Equals(rootRegistryKey, Registry.CurrentUser) || Equals(rootRegistryKey, Registry.LocalMachine) || Equals(rootRegistryKey, Registry.PerformanceData) || Equals(rootRegistryKey, Registry.Users)) && rootRegistryKey.OpenSubKey(rootKey, false) is RegistryKey registryKey && registryKey.GetValue(key) is object getValue)
                 {
                     // 读取布尔值
-                    if (typeof(T) == typeof(bool) || typeof(T) == typeof(bool?))
+                    if (Equals(typeof(T), typeof(bool)) || Equals(typeof(T), typeof(bool?)))
                     {
                         value = (T)(object)Convert.ToBoolean(getValue);
                     }
-                    else if (typeof(T) == typeof(int) || typeof(T) == typeof(int?))
+                    else if (Equals(typeof(T), typeof(int)) || Equals(typeof(T), typeof(int?)))
                     {
                         value = (T)(object)Convert.ToInt32(getValue);
                     }
-                    else if (typeof(T) == typeof(uint) || typeof(T) == typeof(uint?))
+                    else if (Equals(typeof(T), typeof(uint)) || Equals(typeof(T), typeof(uint?)))
                     {
                         value = (T)(object)Convert.ToUInt32(getValue);
                     }
-                    else if (typeof(T) == typeof(long) || typeof(T) == typeof(long?))
+                    else if (Equals(typeof(T), typeof(long)) || Equals(typeof(T), typeof(long?)))
                     {
                         value = (T)(object)Convert.ToInt64(getValue);
                     }
-                    else if (typeof(T) == typeof(ulong) || typeof(T) == typeof(ulong?))
+                    else if (Equals(typeof(T), typeof(ulong)) || Equals(typeof(T), typeof(ulong?)))
                     {
                         value = (T)(object)Convert.ToUInt64(getValue);
                     }
-                    else if (typeof(T) == typeof(string))
+                    else if (Equals(typeof(T), typeof(string)))
                     {
                         value = (T)(object)Convert.ToString(getValue);
                     }
                     else
                     {
-                        value = typeof(T) == typeof(byte[]) || typeof(T) == typeof(string[]) ? (T)getValue : (T)getValue;
+                        value = Equals(typeof(T), typeof(byte[])) || Equals(typeof(T), typeof(string[])) ? (T)getValue : (T)getValue;
                     }
 
                     registryKey.Close();
@@ -78,27 +78,27 @@ namespace PowerTools.Helpers.Root
                 if ((Equals(rootRegistryKey, Registry.ClassesRoot) || Equals(rootRegistryKey, Registry.CurrentConfig) || Equals(rootRegistryKey, Registry.CurrentUser) || Equals(rootRegistryKey, Registry.LocalMachine) || Equals(rootRegistryKey, Registry.PerformanceData) || Equals(rootRegistryKey, Registry.Users)) && rootRegistryKey.CreateSubKey(rootKey, true) is RegistryKey registryKey)
                 {
                     // 存储 32 位整数类型或者布尔值
-                    if (typeof(T) == typeof(bool) || typeof(T) == typeof(int) || typeof(T) == typeof(uint))
+                    if (Equals(typeof(T), typeof(bool)) || Equals(typeof(T), typeof(int)) || Equals(typeof(T), typeof(uint)))
                     {
                         registryKey.SetValue(key, Convert.ToInt32(value), RegistryValueKind.DWord);
                     }
                     // 存储 64 位整数类型
-                    else if (typeof(T) == typeof(long) || typeof(T) == typeof(ulong))
+                    else if (Equals(typeof(T), typeof(long)) || Equals(typeof(T), typeof(ulong)))
                     {
                         registryKey.SetValue(key, Convert.ToInt32(value), RegistryValueKind.QWord);
                     }
                     // 存储字符串类型
-                    else if (typeof(T) == typeof(string))
+                    else if (Equals(typeof(T), typeof(string)))
                     {
                         registryKey.SetValue(key, value, isExpandString ? RegistryValueKind.ExpandString : RegistryValueKind.String);
                     }
                     // 存储二进制数据
-                    else if (typeof(T) == typeof(byte[]))
+                    else if (Equals(typeof(T), typeof(byte[])))
                     {
                         registryKey.SetValue(key, value, RegistryValueKind.Binary);
                     }
                     // 存储字符串数组
-                    else if (typeof(T) == typeof(string[]))
+                    else if (Equals(typeof(T), typeof(string[])))
                     {
                         registryKey.SetValue(key, value, RegistryValueKind.MultiString);
                     }
