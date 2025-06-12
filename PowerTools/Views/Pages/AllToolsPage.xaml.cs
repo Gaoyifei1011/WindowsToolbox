5e788d9f7acfa31a2a60d1462e1c78c86c23394a
高怡飞 <363301617@qq.com>
2025-06-12 17:28:57 +0800

    Add AllToolsPage
    Add AllToolsPage

---
+++ PowerTools/Views/Pages/AllToolsPage.xaml.cs
@@ -0,0 +1,167 @@
+using System;
+using System.Collections.Generic;
+using Windows.UI.Xaml.Controls;
+using PowerTools.Models;
+using PowerTools.Services.Root;
+using PowerTools.Views.Windows;
+
+// 抑制 IDE0060 警告
+#pragma warning disable IDE0060
+
+namespace PowerTools.Views.Pages
+{
+    /// <summary>
+    /// 所有工具页面
+    /// </summary>
+    public sealed partial class AllToolsPage : Page
+    {
+        private List<ControlItemModel> RelaxToolsList { get; } =
+        [
+            new ControlItemModel()
+            {
+                Title = ResourceService.AllToolsResource.GetString("Loaf"),
+                Description = ResourceService.AllToolsResource.GetString("LoafDescription"),
+                ImagePath = "ms-appx:///Assets/ControlIcon/Loaf.png",
+                Tag = "Loaf"
+            }
+        ];
+
+        // 文件工具列表
+        private List<ControlItemModel> FileToolsList { get; } =
+        [
+            new ControlItemModel()
+            {
+                Title = ResourceService.AllToolsResource.GetString("FileManager"),
+                Description = ResourceService.AllToolsResource.GetString("FileManagerDescription"),
+                ImagePath = "ms-appx:///Assets/ControlIcon/FileManager.png",
+                Tag = "FileManager"
+            },
+            new ControlItemModel()
+            {
+                Title = ResourceService.AllToolsResource.GetString("FileCertificate"),
+                Description = ResourceService.AllToolsResource.GetString("FileCertificateDescription"),
+                ImagePath = "ms-appx:///Assets/ControlIcon/FileCertificate.png",
+                Tag = "FileCertificate"
+            },
+            new ControlItemModel()
+            {
+                Title = ResourceService.AllToolsResource.GetString("FileUnlock"),
+                Description = ResourceService.AllToolsResource.GetString("FileUnlockDescription"),
+                ImagePath = "ms-appx:///Assets/ControlIcon/FileUnlock.png",
+                Tag = "FileUnlock"
+            }
+        ];
+
+        // 资源工具列表
+        private List<ControlItemModel> ResourceToolsList { get; } =
+        [
+            new ControlItemModel()
+            {
+                Title = ResourceService.AllToolsResource.GetString("DownloadManager"),
+                Description = ResourceService.AllToolsResource.GetString("DownloadManagerDescription"),
+                ImagePath = "ms-appx:///Assets/ControlIcon/DownloadManager.png",
+                Tag = "DownloadManager"
+            },
+            new ControlItemModel()
+            {
+                Title = ResourceService.AllToolsResource.GetString("IconExtract"),
+                Description = ResourceService.AllToolsResource.GetString("IconExtractDescription"),
+                ImagePath = "ms-appx:///Assets/ControlIcon/IconExtract.png",
+                Tag = "IconExtract"
+            },
+            new ControlItemModel()
+            {
+                Title = ResourceService.AllToolsResource.GetString("PriExtract"),
+                Description = ResourceService.AllToolsResource.GetString("PriExtractDescription"),
+                ImagePath = "ms-appx:///Assets/ControlIcon/PriExtract.png",
+                Tag = "PriExtract"
+            }
+        ];
+
+        // 个性化工具列表
+        private List<ControlItemModel> PersonalizeToolsList { get; } =
+        [
+            new ControlItemModel()
+            {
+                Title = ResourceService.AllToolsResource.GetString("SwitchTheme"),
+                Description = ResourceService.AllToolsResource.GetString("SwitchThemeDescription"),
+                ImagePath = "ms-appx:///Assets/ControlIcon/SwitchTheme.png",
+                Tag = "SwitchTheme"
+            },
+            new ControlItemModel()
+            {
+                Title = ResourceService.AllToolsResource.GetString("ShellMenu"),
+                Description = ResourceService.AllToolsResource.GetString("ShellMenuDescription"),
+                ImagePath = "ms-appx:///Assets/ControlIcon/ShellMenu.png",
+                Tag = "ShellMenu"
+            },
+            new ControlItemModel()
+            {
+                Title = ResourceService.AllToolsResource.GetString("ContextMenuManager"),
+                Description = ResourceService.AllToolsResource.GetString("ContextMenuManagerDescription"),
+                ImagePath = "ms-appx:///Assets/ControlIcon/ContextMenuManager.png",
+                Tag = "ContextMenuManager"
+            }
+        ];
+
+        // 系统工具列表
+        private List<ControlItemModel> SystemToolsList { get; } =
+        [
+            new ControlItemModel()
+            {
+                Title = ResourceService.AllToolsResource.GetString("LoopbackManager"),
+                Description = ResourceService.AllToolsResource.GetString("LoopbackManagerDescription"),
+                ImagePath = "ms-appx:///Assets/ControlIcon/LoopbackManager.png",
+                Tag = "LoopbackManager"
+            },
+            new ControlItemModel()
+            {
+                Title = ResourceService.AllToolsResource.GetString("DriverManager"),
+                Description = ResourceService.AllToolsResource.GetString("DriverManagerDescription"),
+                ImagePath = "ms-appx:///Assets/ControlIcon/DriverManager.png",
+                Tag = "DriverManager"
+            },
+            new ControlItemModel()
+            {
+                Title = ResourceService.AllToolsResource.GetString("UpdateManager"),
+                Description = ResourceService.AllToolsResource.GetString("UpdateManagerDescription"),
+                ImagePath = "ms-appx:///Assets/ControlIcon/UpdateManager.png",
+                Tag = "UpdateManager"
+            },
+            new ControlItemModel()
+            {
+                Title = ResourceService.AllToolsResource.GetString("WinSAT"),
+                Description = ResourceService.AllToolsResource.GetString("WinSATDescription"),
+                ImagePath = "ms-appx:///Assets/ControlIcon/WinSAT.png",
+                Tag = "WinSAT"
+            }
+        ];
+
+        public AllToolsPage()
+        {
+            InitializeComponent();
+        }
+
+        #region 第一部分：所有工具页面——挂载的事件
+
+        /// <summary>
+        /// 点击条目时进入条目对应的页面
+        /// </summary>
+        private void OnItemClicked(object sender, ItemClickEventArgs args)
+        {
+            if (args.ClickedItem is ControlItemModel controlItem && (MainWindow.Current.Content as MainPage).NavigationItemList.Find(item => string.Equals(item.NavigationTag, controlItem.Tag, StringComparison.OrdinalIgnoreCase)) is NavigationModel navigationItem)
+            {
+                if (navigationItem.NavigationPage == typeof(ShellMenuPage))
+                {
+                    (MainWindow.Current.Content as MainPage).NavigateTo(navigationItem.NavigationPage, "ShellMenu");
+                }
+                else
+                {
+                    (MainWindow.Current.Content as MainPage).NavigateTo(navigationItem.NavigationPage);
+                }
+            }
+        }
+
+        #endregion 第一部分：所有工具页面——挂载的事件
+    }
+}
