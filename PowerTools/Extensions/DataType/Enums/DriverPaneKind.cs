213bb2ce883e06cbdd4597ccdc5668851aaf87e2
高怡飞 <363301617@qq.com>
2025-06-18 11:36:33 +0800

    Add driver enum values
    Add driver enum values

---
+++ PowerTools/Extensions/DataType/Enums/DriverPaneKind.cs
@@ -0,0 +1,11 @@
+﻿namespace PowerTools.Extensions.DataType.Enums
+{
+    /// <summary>
+    /// 驱动浮出面板类型
+    /// </summary>
+    public enum DriverPaneKind
+    {
+        UseInstruction = 0,
+        TaskManager = 1,
+    }
+}
