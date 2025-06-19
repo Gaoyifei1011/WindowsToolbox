9c9aa448319b6b704e6feb7a7408b5ab73f43c6c
高怡飞 <363301617@qq.com>
2025-06-20 01:17:56 +0800

    Create ShellEditKind.cs

---
+++ PowerTools/Extensions/DataType/Enums/ShellEditKind.cs
@@ -0,0 +1,11 @@
+﻿namespace PowerTools.Extensions.DataType.Enums
+{
+    /// <summary>
+    /// 编辑菜单类型
+    /// </summary>
+    public enum ShellEditKind
+    {
+        AddMenu = 0,
+        EditMenu = 1
+    }
+}
