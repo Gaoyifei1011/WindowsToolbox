6731760b2219ee84d544ae0a1c0ebe4c29c98ab2
高怡飞 <363301617@qq.com>
2025-06-21 09:35:28 +0800

    Update copy paste method
    Update copy paste method

--- PowerTools/Helpers/Root/CopyPasteHelper.cs
+++ PowerTools/Helpers/Root/CopyPasteHelper.cs
@@ -1,9 +1,6 @@
 ﻿using PowerTools.Services.Root;
 using System;
-using System.Collections.Specialized;
 using System.Diagnostics.Tracing;
-using System.Drawing;
-using System.IO;
 using System.Windows.Forms;
 
 namespace PowerTools.Helpers.Root
@@ -20,57 +17,13 @@ namespace PowerTools.Helpers.Root
         {
             try
             {
-                Windows.ApplicationModel.DataTransfer.DataPackage dataPackage = new()
-                {
-                    RequestedOperation = Windows.ApplicationModel.DataTransfer.DataPackageOperation.Copy,
-                };
-                dataPackage.SetText(content);
-                Windows.ApplicationModel.DataTransfer.Clipboard.SetContent(dataPackage);
+                Clipboard.SetDataObject(content);
                 return true;
             }
             catch (Exception e)
             {
                 LogService.WriteLog(EventLevel.Error, "Copy text to clipboard failed", e);
                 return false;
-            }
-        }
-
-        /// <summary>
-        /// 读取剪贴板中的图片
-        /// </summary>
-        public static Image ReadClipboardImage()
-        {
-            try
-            {
-                IDataObject iData = Clipboard.GetDataObject();
-                if (iData.GetDataPresent(DataFormats.MetafilePict))
-                {
-                    return Clipboard.GetImage();
-                }
-                else if (iData.GetDataPresent(DataFormats.FileDrop))
-                {
-                    StringCollection files = Clipboard.GetFileDropList();
-                    return files.Count is 0 ? null : Image.FromFile(files[0]);
-                }
-                else if (iData.GetDataPresent(DataFormats.Text))
-                {
-                    string path = (string)iData.GetData(DataFormats.Text);
-                    char[] chars = Path.GetInvalidPathChars();
-                    if (path.IndexOfAny(chars) >= 0)
-                    {
-                        return null;
-                    }
-                    return File.Exists(path) ? Image.FromFile(path) : null;
-                }
-                else
-                {
-                    return null;
-                }
-            }
-            catch (Exception e)
-            {
-                LogService.WriteLog(EventLevel.Error, "Read clipboard failed", e);
-                return null;
             }
         }
     }
