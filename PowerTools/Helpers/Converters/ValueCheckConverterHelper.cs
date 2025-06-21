c478d08eaacb635bcaba711f1271a71cd3706731
高怡飞 <363301617@qq.com>
2025-06-21 09:40:48 +0800

    Update ValueCheckConverterHelper.cs

--- PowerTools/Helpers/Converters/ValueCheckConverterHelper.cs
+++ PowerTools/Helpers/Converters/ValueCheckConverterHelper.cs
@@ -8,14 +8,6 @@ namespace PowerTools.Helpers.Converters
     /// </summary>
     public static class ValueCheckConverterHelper
     {
-        /// <summary>
-        /// 确定当前选择的索引是否为目标控件
-        /// </summary>
-        public static Visibility IsCurrentControl(int selectedIndex, int index)
-        {
-            return selectedIndex.Equals(index) ? Visibility.Visible : Visibility.Collapsed;
-        }
-
         public static Visibility IsCurrentItem(object selectedItem, object item)
         {
             return Equals(selectedItem, item) ? Visibility.Visible : Visibility.Collapsed;
