7625599b216930f9ac5fe47f199e7a5213ac10ea
高怡飞 <363301617@qq.com>
2025-07-03 23:05:03 +0800

    Create IconExtractResultKind.cs

---
+++ PowerToolbox/Extensions/DataType/Enums/IconExtractResultKind.cs
@@ -0,0 +1,13 @@
+﻿namespace PowerToolbox.Extensions.DataType.Enums
+{
+    /// <summary>
+    /// 图标提取数据结果模型
+    /// </summary>
+    public enum IconExtractResultKind
+    {
+        Welcome = 0,
+        Parsing = 1,
+        Successfully = 2,
+        Failed = 3
+    }
+}
