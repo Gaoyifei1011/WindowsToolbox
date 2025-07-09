e7f1793ffbc8618d7d8c07cd04db8392324422e5
高怡飞 <363301617@qq.com>
2025-07-09 10:10:25 +0800

    Import Dnsapi
    Import Dnsapi

---
+++ PowerToolbox/WindowsAPI/PInvoke/Dnsapi/DnsapiLibrary.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.InteropServices;
+
+namespace PowerToolbox.WindowsAPI.PInvoke.Dnsapi
+{
+    /// <summary>
+    /// Dnsapi.dll 函数库
+    /// </summary>
+    public static class DnsapiLibrary
+    {
+        private const string Dnsapi = "dnsapi.dll";
+
+        [DllImport(Dnsapi, CharSet = CharSet.Unicode, EntryPoint = "DnsFlushResolverCache", PreserveSig = true, SetLastError = false)]
+        public static extern bool DnsFlushResolverCache();
+    }
+}
