60d03d94a7e4e3e12ddbcd78949393a081a2c69c
高怡飞 <363301617@qq.com>
2025-07-09 10:11:24 +0800

    Add HostsResultKind
    Add HostsResultKind

---
+++ PowerToolbox/Extensions/DataType/Enums/HostsResultKind.cs
@@ -0,0 +1,12 @@
+﻿namespace PowerToolbox.Extensions.DataType.Enums
+{
+    /// <summary>
+    /// Hosts 文件编辑器结果数据模型
+    /// </summary>
+    public enum HostsResultKind
+    {
+        Loading = 0,
+        Failed = 1,
+        Successfully = 2,
+    }
+}
