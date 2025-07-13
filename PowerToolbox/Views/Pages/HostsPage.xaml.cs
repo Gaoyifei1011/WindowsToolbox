8743829ddf880dcd6b6b9b0650119f327ea0f232
高怡飞 <363301617@qq.com>
2025-07-13 16:44:57 +0800

    Update Hosts UI implementation
    Update Hosts UI implementation

--- PowerToolbox/Views/Pages/HostsPage.xaml.cs
+++ PowerToolbox/Views/Pages/HostsPage.xaml.cs
@@ -1,6 +1,8 @@
-﻿using PowerToolbox.Extensions.DataType.Enums;
+﻿using PowerToolbox.Extensions.DataType.Class;
+using PowerToolbox.Extensions.DataType.Enums;
 using PowerToolbox.Models;
 using PowerToolbox.Services.Root;
+using PowerToolbox.Views.Windows;
 using PowerToolbox.WindowsAPI.PInvoke.Dnsapi;
 using System;
 using System.Collections.Generic;
@@ -9,10 +11,13 @@ using System.ComponentModel;
 using System.Diagnostics;
 using System.Diagnostics.Tracing;
 using System.IO;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using Windows.System;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Navigation;
 
 // 抑制 CA1822，IDE0060 警告
@@ -26,9 +31,12 @@ namespace PowerToolbox.Views.Pages
     public sealed partial class HostsPage : Page, INotifyPropertyChanged
     {
         private readonly string hostsPath = Path.Combine(Environment.SystemDirectory, "drivers\\etc\\hosts");
+        private readonly string AvailableString = ResourceService.HostsResource.GetString("Available");
         private readonly string HostsEmptyDescriptionString = ResourceService.HostsResource.GetString("HostsEmptyDescription");
         private readonly string HostsEmptyWithConditionDescriptionString = ResourceService.HostsResource.GetString("HostsEmptyWithConditionDescription");
+        private readonly string NotAvailableString = ResourceService.HostsResource.GetString("NotAvailable");
         private readonly SynchronizationContext synchronizationContext = SynchronizationContext.Current;
+        private bool isAllowClosed = false;
         private FileSystemWatcher fileSystemWatcher;
 
         private HostsResultKind _hostsResultKind;
@@ -79,6 +87,95 @@ namespace PowerToolbox.Views.Pages
             }
         }
 
+        /// <summary>
+        /// 地址
+        /// </summary>
+        private string _addressText;
+
+        public string AddressText
+        {
+            get { return _addressText; }
+
+            set
+            {
+                if (!string.Equals(_addressText, value))
+                {
+                    _addressText = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(AddressText)));
+                }
+            }
+        }
+
+        private string _hostText;
+
+        public string HostText
+        {
+            get { return _hostText; }
+
+            set
+            {
+                if (!string.Equals(_hostText, value))
+                {
+                    _hostText = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(HostText)));
+                }
+            }
+        }
+
+        /// <summary>
+        /// 注释
+        /// </summary>
+        private string _annotationText;
+
+        public string AnnotationText
+        {
+            get { return _annotationText; }
+
+            set
+            {
+                if (!string.Equals(_annotationText, value))
+                {
+                    _annotationText = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(AnnotationText)));
+                }
+            }
+        }
+
+        /// <summary>
+        /// 是否可用
+        /// </summary>
+        private bool _isAvailable;
+
+        public bool IsAvailable
+        {
+            get { return _isAvailable; }
+
+            set
+            {
+                if (!Equals(_isAvailable, value))
+                {
+                    _isAvailable = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsAvailable)));
+                }
+            }
+        }
+
+        private bool _isPrimaryButtonEnabled;
+
+        public bool IsPrimaryButtonEnabled
+        {
+            get { return _isPrimaryButtonEnabled; }
+
+            set
+            {
+                if (!Equals(_isPrimaryButtonEnabled, value))
+                {
+                    _isPrimaryButtonEnabled = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsPrimaryButtonEnabled)));
+                }
+            }
+        }
+
         private List<HostsModel> HostsList { get; } = [];
 
         private ObservableCollection<HostsModel> HostsCollection { get; } = [];
@@ -95,11 +192,11 @@ namespace PowerToolbox.Views.Pages
         /// <summary>
         /// 导航到该页面触发的事件
         /// </summary>
-        protected override void OnNavigatedTo(NavigationEventArgs args)
+        protected override async void OnNavigatedTo(NavigationEventArgs args)
         {
             base.OnNavigatedTo(args);
 
-            Task.Run(() =>
+            await Task.Run(() =>
             {
                 if (fileSystemWatcher is null)
                 {
@@ -120,6 +217,8 @@ namespace PowerToolbox.Views.Pages
                     }
                 }
             });
+
+            await GetHostsAsync();
         }
 
         /// <summary>
@@ -155,7 +254,130 @@ namespace PowerToolbox.Views.Pages
 
         #endregion 第一部分：重写父类事件
 
-        #region 第三部分：网络回环管理页面——挂载的事件
+        #region 第二部分：ExecuteCommand 命令调用时挂载的事件
+
+        /// <summary>
+        /// 编辑 Hosts 文件条目内容
+        /// </summary>
+        private void OnEditExecuteRequested(object sender, ExecuteRequestedEventArgs args)
+        {
+            if (args.Parameter is HostsModel hosts)
+            {
+                AddressText = hosts.Address;
+                HostText = hosts.Hosts;
+                AnnotationText = hosts.Annotation;
+                IsAvailable = hosts.IsAvailable;
+
+                EditHostFlyout.ShowAt(MainWindow.Current.Content, new()
+                {
+                    Placement = FlyoutPlacementMode.Full,
+                    ShowMode = FlyoutShowMode.Standard,
+                });
+            }
+        }
+
+        /// <summary>
+        /// 删除 Hosts 文件条目内容
+        /// </summary>
+        private void OnDeleteExecuteRequested(object sender, ExecuteRequestedEventArgs args)
+        {
+        }
+
+        #endregion 第二部分：ExecuteCommand 命令调用时挂载的事件
+
+        #region 第三部分：Hosts 文件编辑器页面——挂载的事件
+
+        /// <summary>
+        /// 浮出控件关闭时触发的事件
+        /// </summary>
+        private void OnClosing(FlyoutBase sender, FlyoutBaseClosingEventArgs args)
+        {
+            if (isAllowClosed)
+            {
+                isAllowClosed = false;
+            }
+            else
+            {
+                args.Cancel = true;
+            }
+        }
+
+        /// <summary>
+        /// 浮出控件接受屏幕按键触发的事件
+        /// </summary>
+        private void OnFlyoutKeyDown(object sender, global::Windows.UI.Xaml.Input.KeyRoutedEventArgs args)
+        {
+            if (args.Key is VirtualKey.Escape && sender is Grid grid && grid.Tag is Flyout flyout)
+            {
+                isAllowClosed = true;
+                flyout.Hide();
+            }
+        }
+
+        /// <summary>
+        /// 地址文本框内容发生变化触发的事件
+        /// </summary>
+        private void OnAddressTextChanged(object sender, TextChangedEventArgs args)
+        {
+            if (sender is TextBox textBox)
+            {
+                AddressText = textBox.Text;
+                IsPrimaryButtonEnabled = !string.IsNullOrEmpty(AddressText) && !string.IsNullOrEmpty(HostText);
+            }
+        }
+
+        /// <summary>
+        /// 主机文本框内容发生变化触发的事件
+        /// </summary>
+        private void OnHostTextChanged(object sender, TextChangedEventArgs args)
+        {
+            if (sender is TextBox textBox)
+            {
+                HostText = textBox.Text;
+                IsPrimaryButtonEnabled = !string.IsNullOrEmpty(AddressText) && !string.IsNullOrEmpty(HostText);
+            }
+        }
+
+        /// <summary>
+        /// 注释文本框内容发生变化触发的事件
+        /// </summary>
+        private void OnAnnotationTextChanged(object sender, TextChangedEventArgs args)
+        {
+            if (sender is TextBox textBox)
+            {
+                AnnotationText = textBox.Text;
+            }
+        }
+
+        /// <summary>
+        /// 添加 Hosts 文件条目内容
+        /// </summary>
+        private void OnAddClicked(object sender, RoutedEventArgs args)
+        {
+            isAllowClosed = true;
+            AddNewHostFlyout.Hide();
+        }
+
+        /// <summary>
+        /// 更新 Hosts 文件条目内容
+        /// </summary>
+        private void OnUpdateClicked(object sender, RoutedEventArgs args)
+        {
+            isAllowClosed = true;
+            EditHostFlyout.Hide();
+        }
+
+        /// <summary>
+        /// 关闭对话框
+        /// </summary>
+        private void OnCloseClicked(object sender, RoutedEventArgs args)
+        {
+            if (sender is Button button && button.Tag is Flyout flyout)
+            {
+                isAllowClosed = true;
+                flyout.Hide();
+            }
+        }
 
         /// <summary>
         /// 查询搜索内容
@@ -168,7 +390,7 @@ namespace PowerToolbox.Views.Pages
                 HostsCollection.Clear();
                 foreach (HostsModel hostsItem in HostsList)
                 {
-                    if (!string.IsNullOrEmpty(hostsItem.AddRess) && hostsItem.AddRess.Contains(SearchText))
+                    if (!string.IsNullOrEmpty(hostsItem.Address) && hostsItem.Address.Contains(SearchText))
                     {
                         hostsItem.IsSelected = false;
                         HostsList.Add(hostsItem);
@@ -243,7 +465,16 @@ namespace PowerToolbox.Views.Pages
         /// </summary>
         private void OnAddNewHostClicked(object sender, RoutedEventArgs args)
         {
-            // TODO：使用 Flyout 作为对话框
+            AddressText = string.Empty;
+            HostText = string.Empty;
+            AnnotationText = string.Empty;
+            IsAvailable = true;
+
+            AddNewHostFlyout.ShowAt(MainWindow.Current.Content, new()
+            {
+                Placement = FlyoutPlacementMode.Full,
+                ShowMode = FlyoutShowMode.Standard,
+            });
         }
 
         /// <summary>
@@ -286,9 +517,9 @@ namespace PowerToolbox.Views.Pages
             });
         }
 
-        #endregion 第三部分：网络回环管理页面——挂载的事件
+        #endregion 第三部分：Hosts 文件编辑器页面——挂载的事件
 
-        #region 第四部分：网络回环管理页面——自定义事件
+        #region 第四部分：Hosts 文件编辑器页面——自定义事件
 
         /// <summary>
         /// 当更改指定 Path 中的文件和目录时发生的事件
@@ -345,7 +576,7 @@ namespace PowerToolbox.Views.Pages
             }, null);
         }
 
-        #endregion 第四部分：网络回环管理页面——自定义事件
+        #endregion 第四部分：Hosts 文件编辑器页面——自定义事件
 
         /// <summary>
         /// 获取 Hosts 文件内容
@@ -359,6 +590,18 @@ namespace PowerToolbox.Views.Pages
             List<HostsModel> hostsList = await Task.Run(() =>
             {
                 List<HostsModel> hostsList = [];
+                string[] hostsLinesArray = File.ReadAllLines(hostsPath, Encoding.UTF8);
+
+                foreach (string hostsLine in hostsLinesArray)
+                {
+                    if (string.IsNullOrWhiteSpace(hostsLine))
+                    {
+                        continue;
+                    }
+
+                    string trimmedHostLine = hostsLine.Trim();
+                    ParseHostsLine(trimmedHostLine);
+                }
 
                 return hostsList;
             });
@@ -376,6 +619,34 @@ namespace PowerToolbox.Views.Pages
                 {
                 }
             }
+
+            HostsCollection.Add(new HostsModel()
+            {
+                Address = "TestAddress",
+                Annotation = "TestAnnotation",
+                Hosts = "TestHosts",
+                IsSelected = true,
+                IsAvailable = true,
+                IsAvailableString = AvailableString
+            });
+
+            HostsResultKind = HostsResultKind.Successfully;
+        }
+
+        /// <summary>
+        /// 获取加载网络回环是否成功
+        /// </summary>
+        private Visibility GetHostsSuccessfullyState(HostsResultKind hostsResultKind, bool isSuccessfully)
+        {
+            return isSuccessfully ? hostsResultKind is HostsResultKind.Successfully ? Visibility.Visible : Visibility.Collapsed : hostsResultKind is HostsResultKind.Successfully ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        /// <summary>
+        /// 检查搜索网络回环是否成功
+        /// </summary>
+        private Visibility CheckHostsState(HostsResultKind hostsResultKind, HostsResultKind comparedHostsResultKind)
+        {
+            return Equals(hostsResultKind, comparedHostsResultKind) ? Visibility.Visible : Visibility.Collapsed;
         }
 
         /// <summary>
@@ -385,6 +656,16 @@ namespace PowerToolbox.Views.Pages
         private bool GetIsLoading(HostsResultKind hostsResultKind)
         {
             return hostsResultKind is not HostsResultKind.Loading;
+        }
+
+        /// <summary>
+        /// 解析每一行 Hosts 文件内容
+        /// </summary>
+        private HostsModel ParseHostsLine(string line)
+        {
+            string[] hostsLineArray = line.Split(' ');
+
+            return new HostsModel();
         }
     }
 }
