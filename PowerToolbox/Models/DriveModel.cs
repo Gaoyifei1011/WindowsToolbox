9ba43243fba9ed6b11fc51f4057970f03d5102b3
高怡飞 <363301617@qq.com>
2025-07-17 23:48:48 +0800

    Add DriveModel
    Add DriveModel

---
+++ PowerToolbox/Models/DriveModel.cs
@@ -0,0 +1,64 @@
+﻿using System.ComponentModel;
+using System.IO;
+
+namespace PowerToolbox.Models
+{
+    /// <summary>
+    /// 驱动器数据模型
+    /// </summary>
+    public class DriveModel : INotifyPropertyChanged
+    {
+        private bool _isSelected;
+
+        public bool IsSelected
+        {
+            get { return _isSelected; }
+
+            set
+            {
+                if (!Equals(_isSelected, value))
+                {
+                    _isSelected = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsSelected)));
+                }
+            }
+        }
+
+        /// <summary>
+        /// 驱动器名称
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 驱动器空间
+        /// </summary>
+        public string Space { get; set; }
+
+        /// <summary>
+        /// 是否为系统卷
+        /// </summary>
+        public bool IsSytemDrive { get; set; }
+
+        /// <summary>
+        /// 驱动器已使用空间百分比
+        /// </summary>
+        public double DriveUsedPercentage { get; set; }
+
+        /// <summary>
+        /// 驱动器可用空间警告（可用空间在 5% - 10%）
+        /// </summary>
+        public bool IsAvailableSpaceWarning { get; set; }
+
+        /// <summary>
+        /// 存储空间是否不可用（可用空间在 0% - 5%）
+        /// </summary>
+        public bool IsAvailableSpaceError { get; set; }
+
+        /// <summary>
+        /// 磁盘信息
+        /// </summary>
+        public DriveInfo DriverInfo { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
