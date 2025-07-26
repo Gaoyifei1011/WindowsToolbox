103cac21d9e185b83039cabbf7de9d228c9a7b31
怡飞 高 <363301617@qq.com>
2025-07-27 00:14:21 +0800

    Remove unused API
    Remove unused API

--- PowerToolbox/WindowsAPI/PInvoke/User32/User32Library.cs
+++ PowerToolbox/WindowsAPI/PInvoke/User32/User32Library.cs
@@ -160,13 +160,6 @@ namespace PowerToolbox.WindowsAPI.PInvoke.User32
         public static extern short GetKeyState(Keys nVirtKey);
 
         /// <summary>
-        /// 检索 Shell 桌面窗口的句柄。
-        /// </summary>
-        /// <returns>返回值是 Shell 桌面窗口的句柄。 如果没有 Shell 进程，则返回值为 NULL。</returns>
-        [DllImport(User32, CharSet = CharSet.Unicode, EntryPoint = "GetShellWindow", PreserveSig = true, SetLastError = false)]
-        public static extern IntPtr GetShellWindow();
-
-        /// <summary>
         /// 检索指定的系统指标或系统配置设置，同时考虑提供的 DPI。
         /// </summary>
         /// <param name="nIndex">要检索的系统指标或配置设置。</param>
@@ -174,15 +167,6 @@ namespace PowerToolbox.WindowsAPI.PInvoke.User32
         /// <returns>如果该函数成功，则返回值为非零值。如果函数失败，则返回值为零。</returns>
         [DllImport(User32, CharSet = CharSet.Unicode, EntryPoint = "GetSystemMetricsForDpi", PreserveSig = true, SetLastError = false)]
         public static extern int GetSystemMetricsForDpi(SM nIndex, int dpi);
-
-        /// <summary>
-        /// 检索创建指定窗口的线程的标识符，以及创建该窗口的进程（可选）的标识符。
-        /// </summary>
-        /// <param name="hWnd">窗口的句柄。</param>
-        /// <param name="lpdwProcessId">指向接收进程标识符的变量的指针。 如果此参数不为 NULL， 则 GetWindowThreadProcessId 会将进程的标识符复制到变量;否则，它不会。 如果函数失败，则变量的值保持不变。</param>
-        /// <returns>如果函数成功，则返回值是创建窗口的线程的标识符。 如果窗口句柄无效，则返回值为零。</returns>
-        [DllImport(User32, CharSet = CharSet.Unicode, EntryPoint = "GetWindowThreadProcessId", PreserveSig = true, SetLastError = false)]
-        public static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId);
 
         /// <summary>
         /// 检索指定窗口的边框的尺寸。 尺寸以相对于屏幕左上角的屏幕坐标提供。
@@ -301,23 +285,6 @@ namespace PowerToolbox.WindowsAPI.PInvoke.User32
         [DllImport(User32, CharSet = CharSet.Unicode, EntryPoint = "PtInRect", PreserveSig = true, SetLastError = false)]
         [return: MarshalAs(UnmanagedType.Bool)]
         public static extern bool PtInRect(ref RECT lprc, Point pt);
-
-        /// <summary>
-        /// 将消息发布到指定线程的消息队列。 它返回时不等待线程处理消息。
-        /// </summary>
-        /// <param name="idThread">
-        /// 要向其发布消息的线程的标识符。
-        /// 如果指定的线程没有消息队列，该函数将失败。 当线程首次调用某个用户或 GDI 函数时，系统会创建线程的消息队列。 有关详细信息，请参阅“备注”部分。
-        /// 消息发布受 UIPI 的约束。 进程的线程只能将消息发布到较低或等于完整性级别的线程的已发布消息队列。
-        /// 此线程必须具有 SE_TCB_NAME 特权，才能将消息发布到属于具有相同本地唯一标识符（LUID）但位于其他桌面中的进程的线程。 否则，函数将失败并返回 ERROR_INVALID_THREAD_ID。
-        /// 此线程必须与调用线程属于同一桌面，或者属于具有相同 LUID 的进程。 否则，函数将失败并返回 ERROR_INVALID_THREAD_ID。
-        /// </param>
-        /// <param name="Msg">要发布的消息的类型。</param>
-        /// <param name="wParam">其他特定于消息的信息。</param>
-        /// <param name="lParam">其他特定于消息的信息。</param>
-        /// <returns>如果函数成功，则返回值为非零。如果函数失败，则返回值为零。</returns>
-        [DllImport(User32, CharSet = CharSet.Unicode, EntryPoint = "PostThreadMessage", PreserveSig = true, SetLastError = false)]
-        public static extern bool PostThreadMessage(uint idThread, WindowMessage Msg, UIntPtr wParam, IntPtr lParam);
 
         /// <summary>
         /// 创建从指定文件中提取的图标的句柄数组。
