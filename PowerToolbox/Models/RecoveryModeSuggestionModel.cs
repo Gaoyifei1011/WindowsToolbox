866e0cabdd3c923feeaf6854d47aaed666f3aec4
怡飞 高 <363301617@qq.com>
2025-07-28 22:47:32 +0800

    Add RecoveryModeSuggestionModel
    Add RecoveryModeSuggestionModel

---
+++ PowerToolbox/Models/RecoveryModeSuggestionModel.cs
@@ -0,0 +1,23 @@
+﻿namespace PowerToolbox.Models
+{
+    /// <summary>
+    /// 恢复模式建议数据模型
+    /// </summary>
+    public class RecoveryModeSuggestionModel
+    {
+        /// <summary>
+        /// 文件系统
+        /// </summary>
+        public string FileSystem { get; set; }
+
+        /// <summary>
+        /// 使用状况
+        /// </summary>
+        public string Circumstances { get; set; }
+
+        /// <summary>
+        /// 建议模式
+        /// </summary>
+        public string RecommendedMode { get; set; }
+    }
+}
